3a27886d7960140c513998fbc0aab462543cc7e2
SylviaifyX <101458210+SylviaifyX@users.noreply.github.com>
2023-09-15 19:50:58 +0100

    Update router

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,25 @@
-
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  // Outlet,
-} from "react-router-dom";
 import Home from "./Layout/Home/home";
 import Movie_id from "./Layout/Movie_id/movieId"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
-
-// const Layout = () => {
-//   return (
-//     <div>
-//       <Navbar/>
-//       <Outlet/>
-//       <Footer/>
-//     </div>
-//   )
-
-// }
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout/>,
-//     children: [
-//       {
-//         path: "/",
-//         element:<Home/>
-//       },
-//       {
-//          path: "/movies/:id",
-//          element:<Movie_id />
-
-//        }
-//     ]
-//   },
-// ])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: {<Home/>}
+  },
+  {
+    path: "movies/:id",
+    element: :<Movie_id />,
+  },
+]);
 
 function App() {
-
-  return (
-    <>
-      <BrowserRouter>
-        <div>
-          <Routes>
-            <Route path ="/" element ={<Home/>}/>
-            <Route path ="movies/:id"element ={<Movie_id/>}/>
-          </Routes>
-        </div>
-      </BrowserRouter>
-
-    </>
-  )
+  return  <RouterProvider router={router} /> 
 }
 
 export default App
